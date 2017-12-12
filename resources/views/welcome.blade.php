e2e0c6ff818a0b7b267c48792966c67f8e53f565
k9606 <bishengfei@126.com>
2017-12-13 00:42:36 +0800

    使用vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <script src="{{asset('js/vue.js')}}"></script>
         <title>Welcome</title>
 
         <!-- Styles -->
@@ -72,9 +72,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome
+                    @{{ message }}
                 </div>
             </div>
         </div>
     </body>
+    <script>
+        new Vue({
+            el: '.title',
+            data: {
+                message: 'Welcome'
+            }
+        })
+    </script>
 </html>
