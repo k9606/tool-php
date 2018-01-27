0215686afe7fbdba28b3c6937f6082259755218b
k9606 <bishengfei@126.com>
2018-01-27 17:58:52 +0800

    测试命令

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -5,14 +5,14 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
-class GetHtml extends Command
+class TestCommand extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'get:html';
+    protected $signature = 'test:run';
 
     /**
      * The console command description.
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
-        \App\Console\Commands\GetHtml::class
+        \App\Console\Commands\TestCommand::class
     ];
 
     /**
