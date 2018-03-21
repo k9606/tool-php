84a186af4b50bcf3a902bebfad01e459446c2570
k9606 <bishengfei@126.com>
2018-03-21 18:23:32 +0800

    爬图片

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands;
 
+use Goutte\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\DomCrawler\Crawler;
 
 class TestCommand extends Command
 {
@@ -39,46 +41,19 @@ class TestCommand extends Command
     public function handle()
     {
         //
-        $url = 'http://www.80s.tw/ju/list/----12-';
-        $a = $this->curl_request($url);
-        //include
-        $html = new simple_html_dom();
-        Log::info($a);
-        var_dump($a);
+        $client = new \GuzzleHttp\Client();  //忽略SSL错误
+        $response = $client->get("http://t.dyxz.la/upload/img/201803/poster_20180317_8771820_b.jpg", ['save_to' => public_path('/')]);
     }
 
-    public function curl_request($url, $post = '', $cookie_jar = '',$cookie = '',$http = 1)
+    public function getImageUrl()
     {
-        $ch = curl_init(); //初始化curl
-        curl_setopt($ch, CURLOPT_URL, $url); //抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0); //设置header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-        if($cookie){
-            curl_setopt($ch, CURLOPT_COOKIE, $cookie); //设置Cookie信息保存在指定的文件中
-        }else{
-            curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_jar); //设置Cookie信息保存在指定的文件中
-        }
-
-        if($post){
-            curl_setopt($ch, CURLOPT_POST, 1); //post提交方式
-            $post = is_array($post) ? http_build_query($post) : $post;
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        }
-
-        if ($http) {
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        }
-
-        $data = curl_exec($ch);
-        if (curl_errno($ch)) {
-            return curl_error($ch);
-        }
-        curl_close($ch);
-        return $data;
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://m.80s.tw/movie/12-0-0-0-0-0-0');
+        $url = $crawler->filter('img.lazy')->each(function (Crawler $node, $i) {
+            $urlList = $node->attr('data-original');
+            return $urlList;
+        });
+        return $url;
     }
 
 }
--- app/Libraries/Helpers.php
+++ app/Libraries/Helpers.php
@@ -2,7 +2,8 @@
 
 // 格式化打印数组
 if (!function_exists('vpd')) {
-    function vpd($data, $type = '') {
+    function vpd($data, $type = '')
+    {
         if ($type != 2) {
             var_dump($data);
             die();
@@ -10,5 +11,42 @@ if (!function_exists('vpd')) {
             print_r($data);
             die();
         }
+    }
+}
+
+// curl封装
+if (!function_exists('curl_request')) {
+    function curl_request($url, $post = '', $cookie_jar = '', $cookie = '', $http = 1)
+    {
+        $ch = curl_init(); //初始化curl
+        curl_setopt($ch, CURLOPT_URL, $url); //抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0); //设置header
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+        if ($cookie) {
+            curl_setopt($ch, CURLOPT_COOKIE, $cookie); //设置Cookie信息保存在指定的文件中
+        } else {
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_jar); //设置Cookie信息保存在指定的文件中
+        }
+
+        if ($post) {
+            curl_setopt($ch, CURLOPT_POST, 1); //post提交方式
+            $post = is_array($post) ? http_build_query($post) : $post;
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        }
+
+        if ($http) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        }
+
+        $data = curl_exec($ch);
+        if (curl_errno($ch)) {
+            return curl_error($ch);
+        }
+        curl_close($ch);
+        return $data;
     }
 }
