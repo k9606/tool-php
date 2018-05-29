9ff02fec23b5c411915d065d9584f26480908d5d
bishengfei@126.com <19940611hqr>
2018-05-29 11:42:03 +0800

    +路由

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ Route::get('/test', function () {
     return view('test');
 });
 
-Route::group(['namespace' => 'Test'], function () {
-    Route::resource('/send', 'TestController@index');
-});
+Route::get('/send', 'TestController@index');
 
 Route::group(['namespace' => 'Drama'], function () {
     Route::resource('/', 'IndexController@index');
