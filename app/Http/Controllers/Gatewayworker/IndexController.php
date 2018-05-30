80dfde4373e7f4dfdd985b34b53509db80b42533
bishengfei@126.com <19940611hqr>
2018-05-30 15:13:51 +0800

    排除本人

--- app/Http/Controllers/Gatewayworker/IndexController.php
+++ app/Http/Controllers/Gatewayworker/IndexController.php
@@ -14,7 +14,7 @@ class IndexController extends Controller
     public function index(Request $request)
     {
         $msg = $request->input('msg');
-        Gateway::sendToAll($msg);
+        Gateway::sendToAll($msg, '', '7f0000010b5600000001');
 
         return jsonp_msg(200, 'ok', $msg);
     }
